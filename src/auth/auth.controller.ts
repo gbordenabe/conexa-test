bcf5f1e2bd6dd489d126b7d06abf1e57199bf91e
Gabriel Bordenabe <gabrielbordenabe@gmail.com>
2024-08-28 17:03:29 -0300

    login endpoint agregado y funcionando

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { ApiController } from 'src/decorators';
 import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
 
 @ApiController('auth')
 export class AuthController {
@@ -10,5 +11,10 @@ export class AuthController {
   @Post('register')
   register(@Body() registerDto: RegisterDto) {
     return this.authService.register(registerDto);
+  }
+
+  @Post('login')
+  login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import { HashingAdapterService } from './hashing-adapter.service';
 import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
@@ -14,13 +15,30 @@ export class AuthService {
     const { email, password } = registerDto;
 
     const existingUser = await this.userService.findOne(email);
-
     if (existingUser) {
       throw new BadRequestException('Email already in use');
     }
 
     const hashedPassword = await this.hashingAdapterService.hash(password);
-
     return await this.userService.create({ email, password: hashedPassword });
+  }
+
+  async login(loginDto: LoginDto) {
+    const { email, password } = loginDto;
+
+    const user = await this.userService.findOne(email);
+    if (!user) {
+      throw new BadRequestException('Invalid credentials');
+    }
+
+    const isPasswordValid = await this.hashingAdapterService.compare(
+      password,
+      user.password,
+    );
+    if (!isPasswordValid) {
+      throw new BadRequestException('Invalid credentials');
+    }
+
+    return user;
   }
 }
