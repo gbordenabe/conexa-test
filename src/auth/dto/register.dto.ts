260578086a602311366e6998be5107c14a74b5e8
Gabriel Bordenabe <gabrielbordenabe@gmail.com>
2024-08-29 09:53:39 -0300

    Add Swagger API documentation for login, register, and movie endpoints

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,14 +1,18 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator';
 import { Role } from 'src/config/constants';
 
 export class RegisterDto {
+  @ApiProperty({ example: 'prueba@email.com' })
   @IsEmail()
   email: string;
 
+  @ApiProperty({ example: '123456' })
   @IsString()
   @MinLength(6)
   password: string;
 
+  @ApiPropertyOptional({ example: 'guest' })
   @IsString()
   @IsOptional()
   role?: Role;
--- src/movie/dto/create-movie.dto.ts
+++ src/movie/dto/create-movie.dto.ts
@@ -1,25 +1,32 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsNumber, IsOptional, IsPositive, IsString } from 'class-validator';
 
 export class CreateMovieDto {
+  @ApiProperty({ example: 'Title test of Movie' })
   @IsString()
   title: string;
 
+  @ApiProperty({ example: 333 })
   @IsNumber()
   @IsPositive()
   episode_id: number;
 
+  @ApiPropertyOptional({ example: 'It is a period of civil war.' })
   @IsString()
   @IsOptional()
   opening_crawl?: string;
 
+  @ApiPropertyOptional({ example: 'George Lucas' })
   @IsString()
   @IsOptional()
   director?: string;
 
+  @ApiPropertyOptional({ example: 'Gary Kurtz, Rick McCallum' })
   @IsString()
   @IsOptional()
-  ucer?: string;
+  producer?: string;
 
+  @ApiPropertyOptional({ example: '1977-05-25' })
   @IsString()
   @IsOptional()
   release_date?: string;
